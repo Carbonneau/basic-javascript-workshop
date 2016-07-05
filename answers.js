857d77618c0012e64d378029a3ffc09bff4d47e0
Jared Carbonneau <jared.carbonneau@me.com>
2016-07-05 17:59:02 +0000

    I've now done the third excersize!

--- answers.js
+++ answers.js
@@ -17,4 +17,15 @@ function lastChar(aString) {
     return aString[(aString.length - 1)]
 }
 
-console.log(lastChar("Hoorah!"))
+//console.log(lastChar("Hoorah!"))
+
+/* Write a function that takes a string and a number, 
+and returns the character at the position represented by the number. 
+The indexing of number should start at 0. 
+Test your function on a few inputs, including the empty string.*/
+
+function charPosition(aString,number) {
+    return aString[number];
+}
+
+console.log(charPosition("why?",3));

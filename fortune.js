c764a094d648cb55a73e6c732989ab51655a386d
Jared Carbonneau <jared.carbonneau@me.com>
2016-07-05 23:48:36 +0000

    completed fortune teller challenge.

---
+++ fortune.js
@@ -0,0 +1,15 @@
+var quotes = [
+    "A woman's mind is cleaner than a man's: She changes it more often.",
+    "Do not take life too seriously. You will never get out of it alive.",
+    "Always remember that you are absolutely unique. Just like everyone else.",
+    "I believe that if life gives you lemons, you should make lemonade... And try to find somebody whose life has given them vodka, and have a party.",
+    "People who think they know everything are a great annoyance to those of us who do.",
+    "We are all here on earth to help others; what on earth the others are here for I don't know.",
+    "If you haven't got anything nice to say about anybody, come sit next to me.",
+    "I'm sorry, if you were right, I'd agree with you.",
+    "A successful man is one who makes more money than his wife can spend. A successful woman is one who can find such a man.",
+    "I never said most of the things I said."
+]
+
+
+console.log(quotes[(Math.floor(Math.random() * 10))]);

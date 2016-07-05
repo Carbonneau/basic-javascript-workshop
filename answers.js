2d6505978ab108b09e7a1b5ff4b6b95aac086d3e
Jared Carbonneau <jared.carbonneau@me.com>
2016-07-05 17:50:10 +0000

    I've done the second function.

--- answers.js
+++ answers.js
@@ -6,5 +6,15 @@ function firstChar(aString) {
     return aString[0];
 }
 
-console.log(firstChar("YAY!!!"))
+// console.log(firstChar("YAY!!!"))
 
+/* Write a function that takes a string and 
+returns the last character of a string. 
+Test your function on a few inputs, 
+including the empty string. */
+
+function lastChar(aString) {
+    return aString[(aString.length - 1)]
+}
+
+console.log(lastChar("Hoorah!"))

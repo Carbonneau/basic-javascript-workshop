8a964e16c86bb9a746e1eff5fba2e3b3784415d5
Jared Carbonneau <jared.carbonneau@me.com>
2016-07-05 17:44:28 +0000

    I've done the first excersize.

---
+++ answers.js
@@ -0,0 +1,10 @@
+//Write a function that takes a string and 
+//returns the first character of the string. 
+//Test your function on a few inputs, including the empty string.
+
+function firstChar(aString) {
+    return aString[0];
+}
+
+console.log(firstChar("YAY!!!"))
+
